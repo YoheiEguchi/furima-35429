bc2f7fa61f059c208460621b5bf392bcc159b30f
Yohei Eguchi <borrow_or_rob.0222@icloud.com>
2021-05-07 18:24:31 +0900

    Revert "ActiveHash、バリデーション修正"
    This reverts commit 43cb971497b7e5af467ba8c6d8a5acf106c871e6.

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,20 +3,17 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :item_name
     validates :item_text
+    validates :category_id
+    validates :shipping_charge_id
+    validates :shipping_area_id
+    validates :day_to_ship_id
     validates :price
     validates :image
-    with_options numericality: { other_than: 1 } do
-      validates :category_id
-      validates :condition_id
-      validates :shipping_charge_id
-      validates :shipping_area_id
-      validates :day_to_ship_id
-    end
   end
   
   belongs_to :user
   has_one_attached :image
-
+  
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_charge
