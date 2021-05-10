79e241f79d76d8ae1870165fb7a0ab142472f150
Yohei Eguchi <borrow_or_rob.0222@icloud.com>
2021-05-10 15:03:18 +0900

    createアクション実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,19 @@
 class ItemsController < ApplicationController
 before_action :basic_auth
+before_action :authenticate_user!, only: [:new, :create]
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path(@item)
+    else
+      render :new
+    end
+  end
 
   private
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,9 @@
 class Item < ApplicationRecord
 
   with_options presence: true do
-    validates :item_name
-    validates :item_text
-    validates :price
+    validates :item_name, length: { maximum: 40 }
+    validates :item_text, length: { maximum: 1000 }
+    validates :price, format: { with: /\A[0-9]+\z/}, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
     validates :image
     with_options numericality: { other_than: 1 } do
       validates :category_id
@@ -17,9 +17,11 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_charge
   belongs_to :shipping_area
   belongs_to :day_to_ship
+
 end
