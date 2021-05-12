057f498ea8cafe5721844c087b8a31f7450c87b9
Yohei Eguchi <borrow_or_rob.0222@icloud.com>
2021-05-12 19:59:55 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
 before_action :authenticate_user!, only: [:new, :create, :edit]
+before_action :set_item, only: [:show, :edit, :update]
 before_action :user_checked, only: [:edit, :update]
 
   def new
@@ -20,15 +21,12 @@ before_action :user_checked, only: [:edit, :update]
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -40,6 +38,10 @@ before_action :user_checked, only: [:edit, :update]
     unless current_user.id == @item.user_id
       redirect_to root_path
     end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   private
